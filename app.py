02860aa8f744da4575d0ab3925093594855defe9
mariyum22 <sadigill818452@gmail.com>
2025-04-20 03:10:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import torch
 import os
+import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from OpenVoice.api import BaseSpeakerTTS, ToneColorConverter
 from OpenVoice import se_extractor
