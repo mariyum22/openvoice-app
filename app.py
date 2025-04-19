ac2e6669aeac788b7681db1980f5f383dfbaa064
mariyum22 <sadigill818452@gmail.com>
2025-04-20 01:41:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("Upload your voice and let the model convert it to another speaker's
 def load_model():
     model = BaseSpeakerTTS(
         config_path="checkpoints/base_speakers/EN/config.json",
-        device="cpu"
+    device="cpu",
         vocoder_path="checkpoints/converter"     # ✅ This must match your GitHub folder
     )
     return model
