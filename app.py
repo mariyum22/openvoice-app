ebbbbaf0c695334a697bcf49e4ad67800648174a
mariyum22 <sadigill818452@gmail.com>
2025-04-19 22:27:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("Upload your voice and let the model convert it to another speaker's
 # Load the model once
 @st.cache_resource
 def load_model():
-    model = BaseSpeakerTTS(config_path="checkpoints/base_speakers/EN/config.json")
+    model = BaseSpeakerTTS(config_path="checkpoints/base_speakers/EN/config.json", device="cpu")
     model.load_model(
         language="en",
         model_path="checkpoints/base_speakers/EN",      # ✅ This must match your GitHub folder
